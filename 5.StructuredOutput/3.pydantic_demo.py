69557127a21b0ce4b301e0cca722b8feaa2dc176
Foolmannn <mannmagar19@gmail.com>
2026-05-31 16:10:29 +0000

    pydantic demo

--- 5.StructuredOutput/3.pydantic_demo.py
+++ 5.StructuredOutput/3.pydantic_demo.py
@@ -1,5 +1,5 @@
 # Pydantic is a data validation and data parsing libraray for python. It ensures that the data you work with is correct, structured, and type - safe
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr,Field
 
 from typing import Optional
 
@@ -35,23 +35,42 @@ class Student(BaseModel):
     name: str = 'suman' # default values
     age: Optional[int] = None
     email: EmailStr
+    # cgpa:float = Field(gt=0,lt=10)
+    cgpa:float = Field(gt=0,lt=10, default=5 , description="A decimal value representing the cgpa of the students") # we can add default parameter too, and custom descriptions tooo this help LLM to be sure and enough knowledge 
 new_student = {}
 
-student = Student(**new_student)
+# student = Student(**new_student)
 
-print(student.name) # now as the dictation is empty we will get the default values. 
+# print(student.name) # now as the dictation is empty we will get the default values. 
 
 #OPTIONAL VALUES:: 
 
-new_student1 = {'name':'Hari', 'age':22}
+# new_student1 = {'name':'Hari', 'age':22}
 # new_student1 = {'name':'Hari', 'age':'22'} # pydantic use type coercing to convert this 22 string to the int as we have defined age as int . 
 
-student1 = Student(**new_student1)
-print(new_student1)
+# student1 = Student(**new_student1)
+# print(new_student1)
 
-new_student2 = {'email':"abcfasdf"}
+# new_student2 = {'email':"abcfasdf",'cgpa':5}
+# new_student2 = {'email':"abc@gmail.com",'cgpa':15}  Input should be less than 10 [type=less_than, input_value=15, input_type=int]
+
+# So this is how we can use the field functions to add the contraints 
+new_student2 = {'email':"abc@gmail.com",'cgpa':5}
 
 student2 = Student(**new_student2)
 
-print(new_student2)  # so there are builtin validataion for email and much more 
+# print(new_student2)  # so there are builtin validataion for email and much more 
 
+# WE CAN SAVE THIS PYDANTIC OBJECT AS THE DICTIONARY 
+
+student_dict = dict(student2)
+print(student_dict)
+
+print(student_dict['age'])
+
+
+# WE CAN SAVE THIS A JSON TOO 
+
+student_json = student2.model_dump_json()
+
+print(student_json)
