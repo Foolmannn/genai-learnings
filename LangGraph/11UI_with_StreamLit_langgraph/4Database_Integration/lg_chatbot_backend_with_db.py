fc9d307f744753dca82be8096bb0dd2c9dadddf8
Foolmannn <mannmagar19@gmail.com>
2026-08-18 12:00:29 +0000

    ML : MIni batch GD

---
+++ LangGraph/11UI_with_StreamLit_langgraph/4Database_Integration/lg_chatbot_backend_with_db.py
@@ -0,0 +1,40 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage , HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import sqlite3
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)  # this flase flag is to emit the constraint of the sqlite to run on the single thread where it is created . So as we will use it on the multiple threads it is neccessary
+
+ # Checkpointer
+checkpointer = SqliteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+# test : 
+CONFIG = {'configurable': {'thread_id':'thread-1'}}     
+
+result =chatbot.invoke({'messages':[HumanMessage(content='What is my Name ?')]},config=CONFIG)
+print(result)
+
